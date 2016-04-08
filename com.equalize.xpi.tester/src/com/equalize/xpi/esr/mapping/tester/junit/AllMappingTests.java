afe0f665783205e5667ecd3ec2e55c08b4166330
engswee <engswee+git@gmail.com>
2016-04-08 14:28:23 +0800

    Initial JUnit 4 test suite for mapping tests

---
+++ com.equalize.xpi.tester/src/com/equalize/xpi/esr/mapping/tester/junit/AllMappingTests.java
@@ -0,0 +1,11 @@
+package com.equalize.xpi.esr.mapping.tester.junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CustomPlain2PlainTest.class})
+public class AllMappingTests {
+
+}
